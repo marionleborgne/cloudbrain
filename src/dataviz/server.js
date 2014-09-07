9c2183870e31ec880029c223d20083b62e424b64
Ubuntu <ubuntu@ip-172-31-14-182.us-west-1.compute.internal>
2014-09-07 01:53:53 +0000

    add cors, add requirements to package.json

--- src/dataviz/server.js
+++ src/dataviz/server.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 
 app.use(express.static(__dirname + '/static'));
+app.use(cors());
 app.listen(80);
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
