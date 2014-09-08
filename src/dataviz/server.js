49d5afb86fa4f2f330a5fab0bb1fd3a259e3ed82
Pierre Karashchuk <krchtchk@gmail.com>
2014-09-07 17:55:29 -0700

    worked on buttons for different channels

--- src/dataviz/server.js
+++ src/dataviz/server.js
@@ -4,7 +4,7 @@ var cors = require('cors');
 
 app.use(express.static(__dirname + '/static'));
 app.use(cors());
-app.listen(80);
+app.listen(8080);
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
