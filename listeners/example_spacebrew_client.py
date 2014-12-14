b467fbd4bd4d969bbf7df8bb7237544a6353092e
Marion <leborgne.mar@gmail.com>
2014-12-14 14:03:47 -0800

    added example spacebrew client

---
+++ listeners/example_spacebrew_client.py
@@ -0,0 +1,40 @@
+__author__ = 'marion'
+
+__author__ = 'marion'
+
+# add the shared settings file to namespace
+import sys
+from os.path import dirname, abspath
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+import settings
+from spacebrew.spacebrew import SpacebrewApp
+import json
+
+
+class SpacebrewClient(object):
+
+    def __init__(self, name, server):
+
+        # configure the spacebrew client
+        self.brew = SpacebrewApp(name, server=server)
+        self.paths = ['/muse/eeg',
+                      '/muse/acc',
+                      '/muse/elements/experimental/concentration',
+                      '/muse/elements/experimental/mellow']
+
+        for path in self.paths:
+            spacebrew_name = path.split('/')[-1]
+            self.brew.add_subscriber(spacebrew_name, "string")
+            self.brew.subscribe(spacebrew_name, self.handle_value)
+
+    def handle_value(self, string_value):
+        value = json.loads(string_value)
+        print value
+
+    def start(self):
+        self.brew.start()
+
+
+if __name__ == "__main__":
+    sb_client = SpacebrewClient('cloudbrain', settings.CLOUDBRAIN_ADDRESS)
+    sb_client.start()
--- listeners/spacebrew_client.py
+++ listeners/spacebrew_client.py
@@ -3,7 +3,6 @@ __author__ = 'marion'
 # add the shared settings file to namespace
 import sys
 from os.path import dirname, abspath
-
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 import settings
 
@@ -18,34 +17,30 @@ import json
 
 
 class SpacebrewClient(object):
-    def __init__(self, name, server, store_in_cassandra=False):
+
+    def __init__(self, name, server):
+        # get app name and server from query string
+        self.name = name
+        server = server
+
         # configure the spacebrew client
         self.brew = SpacebrewApp(name, server=server)
 
         # cassandra
-        if store_in_cassandra:
-            self.muse_cassandra_repo = CassandraRepository(KEYSPACE, MUSE_COLUMN_FAMILY)
-            self.batches = {}
+        self.muse_cassandra_repo = CassandraRepository(KEYSPACE, MUSE_COLUMN_FAMILY)
+        self.batches = {}
         self.paths = ['/muse/eeg',
                       '/muse/acc',
                       '/muse/elements/experimental/concentration',
                       '/muse/elements/experimental/mellow']
 
         for path in self.paths:
+            self.batches[path] = {}
             spacebrew_name = path.split('/')[-1]
             self.brew.add_subscriber(spacebrew_name, "string")
-            if store_in_cassandra:
-                self.batches[path] = {}
-                self.brew.subscribe(spacebrew_name, self.handle_value_cassandra)
-            else:
-                self.brew.subscribe(spacebrew_name, self.handle_value)
-
+            self.brew.subscribe(spacebrew_name, self.handle_value)
 
     def handle_value(self, string_value):
-        value = json.loads(string_value)
-        print value
-
-    def handle_value_cassandra(self, string_value):
         value = json.loads(string_value)
         path = value[0]
         if path in self.paths:
@@ -66,11 +61,11 @@ class SpacebrewClient(object):
                 timestamp = row_key.split('_')[1]
                 print "column batch stored in cassandra for %s -- %s ms" % (path, timestamp)
 
-
     def start(self):
         self.brew.start()
 
 
+
 if __name__ == "__main__":
-    sb_client = SpacebrewClient('cloudbrain', settings.CLOUDBRAIN_ADDRESS, store_in_cassandra=False)
+    sb_client = SpacebrewClient('cloudbrain', settings.CLOUDBRAIN_ADDRESS)
     sb_client.start()
