9bba70621d789fbbf95530cbdd1731dbab1dd25e
Marion <leborgne.mar@gmail.com>
2015-08-09 23:28:29 -0700

    cleanup :lipstick:

--- cloudbrain/connectors/main.py
+++ cloudbrain/connectors/main.py
@@ -1,56 +1,53 @@
 from cloudbrain.connectors.pika_publisher import PikaPublisher as Publisher
 from cloudbrain.settings import DEVICE_METADATA
-import json
+import argparse
 
+_SUPPORTED_DEVICES = [device['device_name'] for device in DEVICE_METADATA]
 
-_DEVICE_ID = "my_device"
-_DEVICE_NAME = "openbci"
-_HOST = "localhost"
-_BUFFER_SIZE = 10
 
 def parse_args():
- 	
-  parser = argparse.ArgumentsParser()
 
-  parser.add_argument('--user-id', required=True,
-                      help='Make user-id unique to you.')
-  parser.add_argument('--device-name', required=True,
-                      help="Enter device_name: 'openbci', 'muse', 'sensortag', 'mock'")
-  parser.add_argument('--host', default='cloudbrain.rocks',
-                      help="To send data to cloudbrain use 'cloudbrain.rocks'. Otherwise use 'localhost' if running locally")
-  parser.add_argument('--buffer-size', default=10,
-                      help='Change at your own pleasure.')
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument('--userId', required=True,
+                      help="Your unique user ID. For example: 'octopicorn2015'")
+  parser.add_argument('--deviceName', required=True,
+                      help="The name of the device your are sending data from. Supported devices are: %s" %_SUPPORTED_DEVICES)
+  parser.add_argument('--cloudbrain', default='cloudbrain.rocks',
+                      help="The address of the CloudBrain instance you are sending data to.\n"
+                           "Use 'cloudbrain.rocks' to send data to our hosted service. \n"
+                           "Otherwise use 'localhost' if running CloudBrain locally")
+  parser.add_argument('--bufferSize', default=10,
+                      help='Size of the buffer ')
   opts = parser.parse_args()
 
   return opts
 
+
 def main():
-	
-	device_names = [el['device_name'] for el in DEVICE_METADATA]
-	opts = parse_args()
-	device_type = opts.device_name
-	device_id = opts.user_id
-	cloudbrain_addr = opts.host
-	buffer_size = opts.buffer_size
 
-	if opts.device_name = 'muse':
-		from cloudbrain.connectors.muse_connector import MuseConnector as Connector
-	elif opts.device_name = 'openbci':
-		from cloudbrain.connectors.openbci_connector import OpenBCIConnector as Connector
-	elif opts.device_name = 'mock'
-		from cloudbrain.connectors.mock_connector import MockConnector as Connector
-	else:
-		raise Exception("Device type '%s' not supported."%opts.device_name)
+  opts = parse_args()
+  device_name = opts.deviceName
+  device_id = opts.userId
+  cloudbrain_address = opts.cloudbrain
+  buffer_size = opts.bufferSize
 
-	publisher = Publisher(device_type, device_id, cloudbrain_addr)
-  	publisher.connect()
-  	connector = Connector(publisher, buffer_size, device_type)
-  	connector.connectDevice()
-  	connector.start()
+  if device_name == 'muse':
+    from cloudbrain.connectors.muse_connector import MuseConnector as Connector
+  elif device_name == 'openbci':
+    from cloudbrain.connectors.openbci_connector import OpenBCIConnector as Connector
+  elif device_name == 'mock':
+    from cloudbrain.connectors.mock_connector import MockConnector as Connector
+  else:
+    raise Exception("Device type '%s' not supported. Suported devices are:%s" %(device_name, _SUPPORTED_DEVICES))
 
+  publisher = Publisher(device_name, device_id, cloudbrain_address)
+  publisher.connect()
+  connector = Connector(publisher, buffer_size, device_name)
+  connector.connectDevice()
+  connector.start()
 
 
 
 if __name__ == "__main__":
-  
   main()
