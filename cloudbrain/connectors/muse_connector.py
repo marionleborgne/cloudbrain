f9d3b37762a0fd575d54872c592ec81d414025c1
Marion <leborgne.mar@gmail.com>
2015-08-09 23:27:58 -0700

    @jnaulty: start muse-io automatically

--- cloudbrain/connectors/muse_connector.py
+++ cloudbrain/connectors/muse_connector.py
@@ -4,15 +4,12 @@ import time
 import sys
 import json
 
-"""
-To start MuseIO, open a terminal or command prompt and type:
 
-muse-io --osc osc.udp://localhost:9090
-"""
+class _unable_to_start_museIO(Exception):
+  pass
+
 
 class MuseConnector(Connector):
-  
-  
   def __init__(self, publisherInstance, buffer_size, device_type='muse', device_port='9090'):
     """
     
@@ -21,30 +18,39 @@ class MuseConnector(Connector):
     super(MuseConnector, self).__init__(publisherInstance, buffer_size, device_type, device_port)
 
 
-
   def connectDevice(self):
     """
     
     :return:
     """
-    
-    callback_functions = { 'eeg' : self._eeg_callback,
-                           'concentration': self._do_nothing,
-                           'mellow': self._do_nothing,
-                           'horseshoe': self._do_nothing}
 
-    
+    # Start museIO
+    try:
+      pass # read: http://stackoverflow.com/questions/89228/calling-an-external-command-in-python
+      # TODO: run the command muse-io --osc osc.udp://localhost:9090
+    except Exception:
+      # TODO: replace Exception by better exception
+      raise _unable_to_start_museIO(
+        "Unable to start MuseIO. Make sure it is installed or not already running.\n "
+        "Read the Muse docs: https://sites.google.com/a/interaxon.ca/muse-developer-site/museio"
+      )
+
+    callback_functions = {'eeg': self._eeg_callback,
+                          'concentration': self._do_nothing,
+                          'mellow': self._do_nothing,
+                          'horseshoe': self._do_nothing}
+
     self.device = MuseServer(self.device_port, callback_functions)
-    
+
   def start(self):
-     try:
+    try:
       self.device.start()
       while 1:
         time.sleep(1)
-     except KeyboardInterrupt:
-        sys.exit()
-    
-    
+    except KeyboardInterrupt:
+      sys.exit()
+
+
   def _eeg_callback(self, raw_sample):
     """
     Callback function handling Muse samples
@@ -55,9 +61,9 @@ class MuseConnector(Connector):
     data = sample[1]
     message = {"channel_%s" % i: data[i] for i in xrange(4)}
     message['timestamp'] = int(time.time() * 1000000)
-    
+
     self.buffer.write(message)
-    
+
   def _mellow_callback(self, raw_sample):
     """
     Callback function handling Muse samples
@@ -69,8 +75,8 @@ class MuseConnector(Connector):
     message = {"mellow": data[0], 'timestamp': int(time.time() * 1000000)}
 
     self.buffer.write(message)
-      
-      
+
+
   def _do_nothing(self, sample):
     pass
 
