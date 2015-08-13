2f41f8e114c19518516c8d9986dddcc411725bf7
Marion <marion@ebrain.io>
2015-08-13 05:47:32 +0000

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+
+
+def read(fname):
+  """
+  Utility function to read specified file.
+  """
+  path = os.path.join(os.path.dirname(__file__), fname)
+  return open(path).read()
+
+
+
+setup(name="cloudbrain",
+      version="0.0",
+      description="CloudBrain",
+      packages=find_packages(),
+      long_description=read("README.md"))
