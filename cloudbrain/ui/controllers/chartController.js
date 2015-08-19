fc220a808da5ebff6d5c126a827e3a840adb078e
jnaulty <jnaulty@gmail.com>
2015-08-18 23:36:28 -0700

    timestamp addition

--- cloudbrain/ui/controllers/chartController.js
+++ cloudbrain/ui/controllers/chartController.js
@@ -6,13 +6,13 @@
     '$interval',
     '$log',
     function ($scope, $http, $interval, $log) {
-      
 
-    $scope.changeColor = function () {
+
+      $scope.changeColor = function () {
         var color_val = 'rgba(255, 255, 255, 0.8)'
         $scope.chartPolar.options.chart.backgroundColor = color_val;
         $scope.chartBar.options.chart.backgroundColor = color_val;
-      }
+      };
       $scope.getDevices = (function () {
         var url = 'http://datastore.cloudbrain.rocks/devices?callback=JSON_CALLBACK';
         $http.jsonp(url).success(function (data, status, headers) {
@@ -47,13 +47,14 @@
 
 
 
-$scope.url = 'http://datastore.cloudbrain.rocks/data?device_name=openbci&metric=eeg&device_id=marion&callback=JSON_CALLBACK';
-$scope.getData = function (device) {
-  $scope.chartPolar.title.text = device.name + ' ' + device.id;
-  $scope.chartBar.title.text = device.name + ' ' + device.id;
-  $scope.chartStock.title.text = device.name + ' ' + device.id;
-  var metric = 'eeg';
-  $scope.cloudbrain = 'http://datastore.cloudbrain.rocks/data?device_name='+device.name+'&metric='+metric+'&device_id='+device.id+'&callback=JSON_CALLBACK';
+      $scope.url = 'http://datastore.cloudbrain.rocks/data?device_name=openbci&metric=eeg&device_id=marion&callback=JSON_CALLBACK';
+      $scope.getData = function (device) {
+        $scope.chartPolar.title.text = device.name + ' ' + device.id;
+        $scope.chartBar.title.text = device.name + ' ' + device.id;
+        $scope.chartStock.title.text = device.name + ' ' + device.id;
+        var metric = 'eeg';
+        $scope.lastTimestamp = Math.floor(Date.now()/1000);
+        $scope.cloudbrain = 'http://datastore.cloudbrain.rocks/data?device_name='+device.name+'&metric='+metric+'&device_id='+device.id+'&callback=JSON_CALLBACK&start='+$scope.lastTimestamp;
         //initialize series data for charts
         $http.jsonp($scope.cloudbrain)
         .then(function(response){
@@ -68,6 +69,7 @@ $scope.getData = function (device) {
           $http.jsonp($scope.cloudbrain)
           .then(function(response){
             $scope.data = response.data;
+            $scope.lastTimestamp = response.data[response.data.length-1].timestamp;
         //$log.log($scope.chartConfig.series[0]);
         for (var obj in $scope.data){
               //$log.log(obj);
@@ -76,194 +78,194 @@ $scope.getData = function (device) {
                 if (prop != 'timestamp'){
                   //$log.log("data." + prop + "= " + $scope.data[obj][prop]);
                   //$log.log(count);
-                  //$scope.chartStock.series[count].addPoint($scope.data[obj][prop], true, true);
-                 $scope.chartStock.series[count].data.push($scope.data[obj][prop]);
+                  //$scope.StockChart3.series[count].addPoint($scope.data[obj][prop], true, true);
+                    $scope.chartStock.series[count].data.push($scope.data[obj][prop]);
+                    $scope.chartStock.series[count].data = $scope.chartStock.series[count].data.slice(-150);
                   //$log.log($scope.chartConfig.series[count].id);
                   //$log.log($scope.chartConfig.series);
                   count++
               //$log.log($scope.chartConfig.series[0].data);
-            };
-          };
-        };
+            }
+          }
+        }
       },
       function(response){
         $log.log('fail');
       });
-        }, 100);
-      };
-
-     
-
-      $scope.chartStock = {
-        options: {
-          chart: {
-            zoomType: 'x',
-            type: 'spline'
-          },
-          tooltip: {
-            enabled: false
-          },
-          legend: {
-            enabled: true
-          },
-          rangeSelector: {
-            buttons: [{
-              count: 50,
-              type: 'millisecond',
-              text: '5S'
-            }, {
-              count: 300,
-              type: 'millisecond',
-              text: '30S'
-            }, {
-              type: 'all',
-              text: 'All'
-            }],
-            selected: 0,
-            inputEnabled: false
-          },
-
-          navigator: {
-            enabled: true
-          }
-        },
-        series: [],
-        title: {
-          text: 'EEG'
-        },
-        useHighStocks: true
-      };
+        }, 50);
+};
 
 
 
+$scope.chartStock = {
+  options: {
+    chart: {
+      zoomType: 'x',
+      type: 'spline'
+    },
+    tooltip: {
+      enabled: false
+    },
+    legend: {
+      enabled: true
+    },
+    rangeSelector: {
+      buttons: [{
+        count: 50,
+        type: 'millisecond',
+        text: '5S'
+      }, {
+        count: 300,
+        type: 'millisecond',
+        text: '30S'
+      }, {
+        type: 'all',
+        text: 'All'
+      }],
+      selected: 0,
+      inputEnabled: false
+    },
 
-
-      $scope.chartPolar = {
-        options: {
-          chart: {
-            polar: true,
-            type: 'spline',
-            margin: [
-            50,
-            50,
-            50,
-            50
-            ]
-          },
-          pane: {
-            size: '80%'
-          },
-          tooltip: {
-            shared: true,
-            pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
-          },
-          legend: {
-            align: 'right',
-            verticalAlign: 'top',
-            layout: 'vertical'
-          }
-        },
-        title: {
-          text: 'EEG',
-          x: - 80
-        },
-        xAxis: {
-          categories: [
-          'Gamma',
-          'Delta',
-          'Theta',
-          'Beta',
-          'Alpha'
-          ],
-          tickmarkPlacement: 'on',
-          lineWidth: 0
-        },
-        yAxis: {
-          gridLineInterpolation: 'polygon',
-          lineWidth: 0,
-          min: 0
-        },
-        series: [
-        {
-          name: 'Device 1',
-          data: [
-          43000,
-          19000,
-          60000,
-          35000,
-          17000
-          ],
-          pointPlacement: 'on',
-          marker: {
-            enabled: false,
-            symbol: 'circle'
-          }
-        }
-        ]
-      };
-      $scope.chartBar = {
-        options: {
-          chart: {
-            margin: [
-            50,
-            50,
-            50,
-            50
-            ],
-            type: 'column'
-          },
-          pane: {
-            size: '80%'
-          },
-          tooltip: {
-            shared: true,
-            pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
-          },
-          legend: {
-            align: 'right',
-            verticalAlign: 'top',
-            y: 70,
-            layout: 'vertical'
-          }
-        },
-        title: {
-          text: 'EEG',
-          x: - 80
-        },
-        xAxis: {
-          categories: [
-          'Gamma',
-          'Delta',
-          'Theta',
-          'Beta',
-          'Alpha'
-          ],
-          tickmarkPlacement: 'on',
-          lineWidth: 0
-        },
-        yAxis: {
-          gridLineInterpolation: 'polygon',
-          lineWidth: 0,
-          min: 0
-        },
-        series: [
-        {
-          name: 'Device 1',
-          data: [
-          43000,
-          19000,
-          60000,
-          35000,
-          17000
-          ],
-          pointPlacement: 'on',
-          marker: {
-            enabled: false,
-            symbol: 'circle'
-          }
-        }
-        ]
-      };
+    navigator: {
+      enabled: true
     }
-    ]);
+  },
+  series: [],
+  title: {
+    text: 'EEG'
+  },
+  useHighStocks: true
+};
+
+
+
+
+$scope.chartPolar = {
+  options: {
+    chart: {
+      polar: true,
+      type: 'spline',
+      margin: [
+      50,
+      50,
+      50,
+      50
+      ]
+    },
+    pane: {
+      size: '80%'
+    },
+    tooltip: {
+      shared: true,
+      pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
+    },
+    legend: {
+      align: 'right',
+      verticalAlign: 'top',
+      layout: 'vertical'
+    }
+  },
+  title: {
+    text: 'EEG',
+    x: - 80
+  },
+  xAxis: {
+    categories: [
+    'Gamma',
+    'Delta',
+    'Theta',
+    'Beta',
+    'Alpha'
+    ],
+    tickmarkPlacement: 'on',
+    lineWidth: 0
+  },
+  yAxis: {
+    gridLineInterpolation: 'polygon',
+    lineWidth: 0,
+    min: 0
+  },
+  series: [
+  {
+    name: 'Device 1',
+    data: [
+    43000,
+    19000,
+    60000,
+    35000,
+    17000
+    ],
+    pointPlacement: 'on',
+    marker: {
+      enabled: false,
+      symbol: 'circle'
+    }
+  }
+  ]
+};
+$scope.chartBar = {
+  options: {
+    chart: {
+      margin: [
+      50,
+      50,
+      50,
+      50
+      ],
+      type: 'column'
+    },
+    pane: {
+      size: '80%'
+    },
+    tooltip: {
+      shared: true,
+      pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
+    },
+    legend: {
+      align: 'right',
+      verticalAlign: 'top',
+      y: 70,
+      layout: 'vertical'
+    }
+  },
+  title: {
+    text: 'EEG',
+    x: - 80
+  },
+  xAxis: {
+    categories: [
+    'Gamma',
+    'Delta',
+    'Theta',
+    'Beta',
+    'Alpha'
+    ],
+    tickmarkPlacement: 'on',
+    lineWidth: 0
+  },
+  yAxis: {
+    gridLineInterpolation: 'polygon',
+    lineWidth: 0,
+    min: 0
+  },
+  series: [
+  {
+    name: 'Device 1',
+    data: [
+    43000,
+    19000,
+    60000,
+    35000,
+    17000
+    ],
+    pointPlacement: 'on',
+    marker: {
+      enabled: false,
+      symbol: 'circle'
+    }
+  }
+  ]
+};
+}
+]);
 }) ();
