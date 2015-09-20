2c5e75484c300e3eda63a987d8940a8722a5b468
Marion <leborgne.mar@gmail.com>
2015-09-20 03:23:44 -0700

    updated cassandra schema with tags and aggregates

--- cloudbrain/datastore/generate_cassandra_schema.py
+++ cloudbrain/datastore/generate_cassandra_schema.py
@@ -4,7 +4,8 @@ from cloudbrain.settings import (DEVICE_METADATA,
                                  USERS_CONSENT_TABLE,
                                  SENSOR_DATA_KEYSPACE,
                                  ANALYTICS_KEYSPACE,
-                                 TAGS_TABLE_NAME)
+                                 TAGS_TABLE_NAME,
+                                 AGGREGATES_TABLE_NAME)
 
 # template for keyspace creation
 _CREATE_KEYSPACE = ("DROP KEYSPACE %s;\n"
@@ -78,6 +79,7 @@ def _create_sensor_data_keyspace_and_table(outFile):
                                           SENSOR_DATA_KEYSPACE,
                                           SENSOR_DATA_KEYSPACE)
     outFile.write(create_keyspace)
+    outFile.write("\n")
 
     for device in DEVICE_METADATA:
 
@@ -96,7 +98,7 @@ def _create_sensor_data_keyspace_and_table(outFile):
             # and metric name
             column_family_name = "%s_%s" % (device_name, metric_name)
             create_column_family = create_column_family_template % (
-            column_family_name, columns)
+                column_family_name, columns)
             outFile.write(create_column_family)
 
         _create_device_metadata_table(outFile, device_name)
@@ -105,45 +107,29 @@ def _create_sensor_data_keyspace_and_table(outFile):
 
 
 
-def create_analytics_keyspace_and_table(outFile):
+def _create_analytics_keyspace_and_table(outFile):
     """
     Create analytics keyspace and tables
     @param outFile: file to write CQL statements to
     """
-    # templates for column family creation
-    create_column_family_template = ("CREATE TABLE %s (device_id text, "
-                                     "timestamp timestamp, %s "
-                                     "PRIMARY KEY (device_id, timestamp)); \n")
-    column_template = 'channel_%s double, '
-
     create_keyspace = _CREATE_KEYSPACE % (ANALYTICS_KEYSPACE,
                                           ANALYTICS_KEYSPACE,
                                           ANALYTICS_KEYSPACE)
+
+    create_tags_table = ("CREATE TABLE %s (tag_id text, tag_name text, user_id text, "
+                         "start timestamp, end timestamp, metadata text, "
+                         "PRIMARY KEY (tag_id));\n") % TAGS_TABLE_NAME
+
+    create_aggregates_table = ("CREATE TABLE %s (aggregate_id text, user_id text, "
+                               "tag_id text, device_type text, aggregate_type text, "
+                               "aggregate_value float, metric text, start timestamp, end timestamp, "
+                               "PRIMARY KEY (user_id));\n") % AGGREGATES_TABLE_NAME
+
     outFile.write(create_keyspace)
-
-    for device in DEVICE_METADATA:
-
-        device_name = device['device_name']
-
-        metrics = device['metrics']
-        for metric in metrics:
-            metric_name = metric['metric_name']
-            num_arguments = metric['num_channels']
-
-            columns = column_template % 0
-            for i in range(1, num_arguments):
-                columns = "%s%s" % (columns, column_template % i)
-
-            # The column family name is a compound of the device name
-            # and metric name
-            column_family_name = "%s_%s" % (device_name, metric_name)
-            create_column_family = create_column_family_template % (
-            column_family_name, columns)
-            outFile.write(create_column_family)
-
-        _create_device_metadata_table(outFile, device_name)
-
-        outFile.write("\n")
+    outFile.write("\n")
+    outFile.write(create_tags_table)
+    outFile.write(create_aggregates_table)
+    outFile.write("\n")
 
 
 
@@ -154,6 +140,7 @@ def main():
     with open('cassandra_schema.cql', 'w') as f:
         _create_sensor_data_keyspace_and_table(f)
         _create_users_keyspace_and_table(f)
+        _create_analytics_keyspace_and_table(f)
 
         schema_path = realpath('cassandra_schema.cql')
         print "\nSUCCESS: Schema generated. Now run: cqlsh -f %s" % schema_path
