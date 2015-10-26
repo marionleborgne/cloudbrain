6b345d80b7f6456bae5666ac646435721752c15e
Ubuntu <ubuntu@ip-172-31-15-118.us-west-1.compute.internal>
2015-10-26 06:31:31 +0000

    parsing requirement.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 import os
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements("requirements.txt")
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
 
 
 
@@ -16,6 +26,7 @@ setup(name="cloudbrain",
       version="0.2.1",
       description="Platform for real-time sensor data analysis and visualization.",
       packages=find_packages(),
+      install_requires=reqs,
       include_package_data=True,
       long_description=read("README.md"),
       license='GNU Affero General Public License v3',
