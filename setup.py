4642d609b0da5f5c30b1f32e337f667897c662cb
Pierre Karashchuk <krchtchk@gmail.com>
2015-11-08 20:01:50 -0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pip.req import parse_requirements
 
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt")
+install_reqs = parse_requirements("requirements.txt", session=False)
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
