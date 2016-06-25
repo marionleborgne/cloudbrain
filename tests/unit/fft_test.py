b891d77d61bed6321272165e7d1d607f1946de70
Marion <leborgne.mar@gmail.com>
2016-06-25 02:49:24 -0400

    renamed transformers module to transforms

--- tests/unit/fft_test.py
+++ tests/unit/fft_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import unittest
 
-from cloudbrain.modules.transformers.fft import FrequencyBandTransformer
+from cloudbrain.modules.transforms.fft import FrequencyBandTransformer
 
 
 
