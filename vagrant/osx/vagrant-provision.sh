48b2604cc7693c0631dbb5995d80b9bf2cb91ac6
Marion <leborgne.mar@gmail.com>
2016-08-20 22:56:06 -0700

    simplifed provisioning scripts

--- vagrant/osx/vagrant-provision.sh
+++ vagrant/osx/vagrant-provision.sh
@@ -1,27 +1,15 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                                                       
 
-# Script used to provision additional requirements not satisfied by image alone.
-# Arguments:
-#   1) node version (i.e. "5.10.0")
+# Script used to provision additional requirements not satisfied by image alone.                                                                                                  
 
 set -o verbose
 set -o xtrace
 
-# Update brew
+# Update brew                                                                                                                                                                     
 rm /usr/local/share/man/man1/brew-cask.1
 sudo -u vagrant -i brew tap --repair
 sudo -u vagrant -i brew update
 
-# Initialize .bashrc with PATH
+# Initialize .bashrc with PATH                                                                                                                                                    
 sudo -u vagrant /usr/libexec/path_helper -s >> /Users/vagrant/.bashrc
 sudo -u vagrant ln -s .bashrc .bash_profile
-
-# Install NVM
-sudo -u vagrant -i brew uninstall node
-sudo -u vagrant -i brew install nvm
-sudo -u vagrant mkdir /Users/vagrant/.nvm
-sudo -u vagrant echo "export NVM_DIR=/Users/vagrant/.nvm" >> /Users/vagrant/.bashrc
-sudo -u vagrant echo "source $(brew --prefix nvm)/nvm.sh" >> /Users/vagrant/.bashrc
-
-# Switch nodejs to version specified
-sudo -u vagrant -i nvm install v$1
