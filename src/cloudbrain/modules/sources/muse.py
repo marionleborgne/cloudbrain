568c23f53044573544bf22984ed23a5805e29e2e
Marion <leborgne.mar@gmail.com>
2016-11-05 21:08:34 -0700

    removed muse_py2 source module

--- src/cloudbrain/modules/sources/muse.py
+++ src/cloudbrain/modules/sources/muse.py
@@ -1,9 +1,7 @@
-import json
 import logging
 import time
-import sys
 
-from cloudbrain.connectors.muse_py2 import MuseConnector
+from cloudbrain.connectors.muse import MuseConnector
 from cloudbrain.modules.interface import ModuleInterface
 
 _LOGGER = logging.getLogger(__name__)
@@ -31,11 +29,12 @@ class MuseSource(ModuleInterface):
         callback_functions = {}
 
         for publisher in self.publishers:
-            metrics_to_num_channels = publisher.metrics_to_num_channels()
-            for (metric_name, num_channels) in metrics_to_num_channels.items():
-                if metric_name not in callback_functions:
-                    callback_functions[metric_name] = self.callback_factory(
-                        metric_name, num_channels)
+            for routing_key, metric_buffer in publisher.metric_buffers.items():
+                # Note: routing_key is "user_key:metric_name" here.
+                if routing_key not in callback_functions:
+                    callback_functions[routing_key] = self.callback_factory(
+                        metric_buffer.name, metric_buffer.num_channels,
+                        publisher)
 
         connector = MuseConnector(ip=self.ip,
                                   port=self.port,
@@ -43,32 +42,25 @@ class MuseSource(ModuleInterface):
                                   callback_functions=callback_functions)
 
         _LOGGER.info('Starting muse connector ...')
-        try:
-            connector.start()
-            while 1:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            sys.exit()
+        connector.start()
 
 
-    def callback_factory(self, metric_name, num_channels):
+    def callback_factory(self, metric_name, num_channels, publisher):
         """
         Callback function generator
         :return: callback function
         """
 
 
-        def callback(sample):
+        def callback(*data):
             """
             Handle muse samples for this metric
-            :param sample: the muse sample to handle
             """
-            data = json.loads(sample)[1]
-            message = {"channel_%s" % i: data[i] for i in xrange(num_channels)}
-            message['timestamp'] = int(time.time() * 1000000)  # micro seconds
-
-            for publisher in self.publishers:
-                publisher.publish(metric_name, message)
+            data = data[1:] # the first element is the OSC path
+            message = {'timestamp': int(time.time() * 1000000)}  # microseconds
+            for i in range(num_channels):
+                message["channel_%s" % i] = data[i]
+            publisher.publish(metric_name, message)
 
 
         return callback
