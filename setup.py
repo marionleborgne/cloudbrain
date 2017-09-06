20318acc7f4aecbfc39d6e8a878f7e80020f2e44
Marion Le Borgne <leborgne.mar@gmail.com>
2017-09-06 17:52:59 +0200

    update for pypi

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@ from setuptools import setup, find_packages
 from pip.download import PipSession
 from pip.req import parse_requirements
 
+VERSION = '0.0.7'
+URL = 'https://github.com/cloudbrain/cloudbrain'
+DOWNLOAD_URL='%s/archive/%s.tar.gz' % (URL, VERSION)
+
 install_reqs = parse_requirements('requirements.txt', session=PipSession())
 
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='cloudbrain',
-      version='0.0.6',
-      description='CloudBrain',
+      version=VERSION,
+      description='Platform for wearable data analysis.',
       author='Marion Le Borgne',
-      url='https://github.com/cloudbrain/cloudbrain',
+      url=URL,
+      download_url=DOWNLOAD_URL,
       package_dir={'': 'src'},
       packages=find_packages('src'),
       install_requires=reqs,
