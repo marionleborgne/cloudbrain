2fe848ec4acd554585329a8e692ea46f5315bbb7
Marion Le Borgne <leborgne.mar@gmail.com>
2017-10-08 04:37:55 -0700

    ADD: MQTT source

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ import os
 # Get __version__ and set other constants.
 # Source: https://stackoverflow.com/a/16084844
 with open(os.path.join('src', 'cloudbrain', 'version.py'), 'r') as f:
-    exec(f.read())
+    exec (f.read())
 URL = 'https://github.com/cloudbrain/cloudbrain'
-DOWNLOAD_URL='%s/archive/%s.tar.gz' % (URL, __version__)
+DOWNLOAD_URL = '%s/archive/%s.tar.gz' % (URL, __version__)
 DESCRIPTION = open('README.rst').read()
+
 
 # Helper function for requirements parsing by requirement type
 def parse_reqs(req_type):
@@ -17,6 +18,7 @@ def parse_reqs(req_type):
     install_reqs = parse_requirements(reqs_file, session=PipSession())
     reqs = [str(ir.req) for ir in install_reqs]
     return reqs
+
 
 # Get requirements for all types
 REQUIREMENT_TYPES = ['core', 'analytics', 'muse']
@@ -44,8 +46,8 @@ setup(name='cloudbrain',
           'muse:python_version>="3"': reqs['muse'],
           'analytics': reqs['analytics']
       },
-      entry_points = {
-        'console_scripts':
-            ['cloudbrain=cloudbrain.run:main']
-        }
+      entry_points={
+          'console_scripts':
+              ['cloudbrain=cloudbrain.run:main']
+      }
       )
--- src/cloudbrain/modules/sinks/mqtt.py
+++ src/cloudbrain/modules/sinks/mqtt.py
@@ -10,29 +10,29 @@ from cloudbrain.modules.interface import ModuleInterface
 _LOGGER = logging.getLogger(__name__)
 
 
-def _convert_old_chunk(data_to_send, num_channels):
+def _convert_old_chunk_to_new_chunk(old_chunk, num_channels):
     """
-    Convert old data chunk to chunk with new data model.
+    Convert old data chunk to new chunk (i.e. with new data model).
     More on the new data model: https://github.com/NeuroJS/
     eeg-stream-data-model/issues/1#issuecomment-309515243
 
-    :param data_to_send: (list of dict)
+    :param old_chunk: (list of dict)
         Chunk of data with the old data model
     :param num_channels: (int)
         Number of channels of the input stream.
-    :return chunk: (dict)
+    :return new_chunk: (dict)
         Chunk of data with the new data model.
     """
-    chunk = []
-    for data in data_to_send:
-        num_channels = len(data) - 1  # don't count the 'timestamp' key
-        sample = {
-            'timestamp': data['timestamp'],
-            'data': [data['channel_%s' % i] for i in range(num_channels)]
+    new_chunk = []
+    for old_sample in old_chunk:
+        num_channels = len(old_sample) - 1  # don't count the 'timestamp' key
+        new_sample = {
+            'timestamp': old_sample['timestamp'],
+            'data': [old_sample['channel_%s' % i] for i in range(num_channels)]
         }
-        chunk.append(sample)
+        new_chunk.append(new_sample)
 
-    return {'chunk': chunk}
+    return {'chunk': new_chunk}
 
 
 def _get_vhost(username, password):
@@ -53,18 +53,20 @@ def _setup_mqtt_channel(username, password, host, vhost, exchange):
     return channel
 
 
-MQTTPublisher = namedtuple('MQTTPublisher',
-                           ['host', 'username', 'password', 'vhost', 'channel'])
+MQTTConnection = namedtuple('MQTTConnection',
+                            ['host', 'username', 'password', 'vhost',
+                             'channel'])
 
 
-class MQTTSink(ModuleInterface):
+class MQTTConverterSink(ModuleInterface):
     """
     This module subscribes to an AMQP stream, converts it into the
     new data model and publishes it to a MQTT topic with a chosen routing key.
     """
+
     def __init__(self, subscribers, publishers, mqtt_routing_key):
 
-        super(MQTTSink, self).__init__(subscribers, publishers)
+        super(MQTTConverterSink, self).__init__(subscribers, publishers)
         _LOGGER.debug("Subscribers: %s" % self.subscribers)
         _LOGGER.debug("Publishers: %s" % self.publishers)
 
@@ -84,18 +86,18 @@ class MQTTSink(ModuleInterface):
                 vhost = _get_vhost(username, password)
                 channel = _setup_mqtt_channel(username, password,
                                               host, vhost, self.exchange)
-                mqtt_publisher = MQTTPublisher(host, username,
-                                               password, vhost, channel)
-                callback = self.callback_factory(num_channels, mqtt_publisher)
+                mqtt_connection = MQTTConnection(host, username,
+                                                 password, vhost, channel)
+                callback = self.callback_factory(num_channels, mqtt_connection)
                 subscriber.subscribe(metric_name, callback)
 
-    def callback_factory(self, num_channels, mqtt_publisher):
+    def callback_factory(self, num_channels, mqtt_connection):
 
         def callback(unused_ch, unused_method, unused_properties, body):
-            data = json.loads(body)
-            chunk = _convert_old_chunk(data, num_channels)
-            mqtt_publisher.channel.basic_publish(exchange=self.exchange,
-                                                 routing_key=self.routing_key,
-                                                 body=json.dumps(chunk))
+            old_chunk = json.loads(body)
+            new_chunk = _convert_old_chunk_to_new_chunk(old_chunk, num_channels)
+            mqtt_connection.channel.basic_publish(exchange=self.exchange,
+                                                  routing_key=self.routing_key,
+                                                  body=json.dumps(new_chunk))
 
         return callback
